681cc1c8aa43c2b79d95d37cdb33837b3d4b9381
ClaytonStamper <claystamper@gmail.com>
2018-07-04 07:26:26 -0500

    render texture added and linked to the animated camera

---
+++ Assets/_Scripts/AnimationCorrection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCorrection : MonoBehaviour {
+
+	Animator anim;
+
+	void Start(){
+
+		anim = GetComponent <Animator> ();
+
+	}
+
+	public void CorrectRotation(){
+
+		anim.SetTrigger ("normalize");
+
+	}
+}
